dc043a55831cf7bbe78da12fd0f0dd672544a56e
Maueen-Njango <maureennjangombira@gmail.com>
2024-04-06 22:45:49 +0300

    add changes

--- rev.js
+++ rev.js
@@ -35,3 +35,58 @@ class Rectangle{
 const rectangle = new Rectangle(10,19);
 const area = rectangle.calculateArea();
 console.log(`Rectangle area:${area}`);
+
+
+
+
+
+///write a  program that creates a class 'book' with properties for title ,author and publication year .
+///Include a function to display book .
+class Book{
+    constructor(title,author,publicationyear) {
+        this.title = title;
+        this.author = author;
+        this.publicationyear = publicationyear;
+
+    }
+    displayDetails(){
+        console.log(`The ${this.title}  is written by ${this.author} in ${this.publicationyear}`)
+    }
+
+    
+
+}
+const book1 = new Book ("stupid goat", "maureen", 1998)
+book1.displayDetails()
+
+//Write a program that creates a class car with properties make,model, and year .Include a function to display car information
+class Car{
+    constructor(make,model,year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    displayDetails() {
+        console.log(`This car ${this.make} is from the ${this.model} created ${this.year}`)
+    }
+
+}
+const Car1 = new Car ("audi", "german machine", 2015)
+Car1.displayDetails()
+
+//write a program that creates a class student with properties for name , age and age.Include a function to display students information
+class student{
+    constructor(name, age,grade){
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+
+    }
+    displayDetails() {
+        console.log(`The students name is ${this.name} who is aged ${this.age}. She scored a ${this.grade} in her main exam`)
+
+    }
+
+}
+const student1 = new student ("maureen", 23, "A")
+student1.displayDetails()
--- rev.py
+++ rev.py
@@ -1,3 +1,15 @@
-#Write a javascript program to create a class called 'rectangle' with properties for width and height.Include two methods to calculate rectangle area and perimeeter 
-#create an instance of the 'rectangle,class and calculate its area and perimeter
 
+
+#Write a Python program to get a list, sorted in increasing order by the last element in each tuple from a given list of non-empty tuples.
+
+  
+ def last(n):
+    return n[-1]
+
+# Define a function called 'sort_list_last' that takes a list of tuples 'tuples' as input
+def sort_list_last(tuples):
+    # Sort the list of tuples 'tuples' using the 'last' function as the key for sorting
+    return sorted(tuples, key=last)
+
+# Call the 'sort_list_last' function with a list of tuples as input and print the sorted result
+print(sort_list_last([(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]))
